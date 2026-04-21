a2818a599632ea9f352600ac15a4ee13dfd6c69e
Chidiogo <chidiogoezeh.me@gmail.com>
2026-04-21 16:34:10 +0100

    updated package.json to include seed in scripts, uploaded seed.js file to sceipts

--- schema.sql
+++ schema.sql
@@ -1,12 +1,14 @@
-CREATE TABLE IF NOT EXISTS profiles (
+DROP TABLE IF EXISTS profiles;
+
+CREATE TABLE profiles (
     id CHAR(36) PRIMARY KEY,
     name VARCHAR(255) NOT NULL UNIQUE,
     gender VARCHAR(50),
     gender_probability FLOAT,
     age INT,
     age_group VARCHAR(20),
-    country_id VARCHAR(2), -- Exact length requirement
-    country_name VARCHAR(255), -- Added requirement
+    country_id VARCHAR(2),
+    country_name VARCHAR(255), 
     country_probability FLOAT,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
